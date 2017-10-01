cf8e5486015fe6a57bba6bdcd770e3935004e8e4
Angelique Bey <angelbey1025@gmail.com>
2017-09-30 21:05:08 -0500

    changed name of department and created variables and used math to print statements in the console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log("This file is linked!");
+var teacher = "April May";
+var department = "Botany";
+var rating1 = 4.5;
+var rating2 = 4.0;
+var rating3 = 4.5;
+var rating4 = 4.8;
+var rating5 = 4.2;
+var avgRating = (rating1 + rating2 + rating3 + rating4 + rating5)/5;
+var ratings = ["4.5", "4.0", "4.5", "4.8", "4.2"];
+console.log("Teacher: " + teacher);
+console.log("Department: " + department);
+console.log("Ratings: " + ratings.join(", "));
+console.log("Avg Rating: " + avgRating);
