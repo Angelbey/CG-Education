7c1e8a2eadab855504356f5640861e537e43c589
Angelique Bey <angelbey1025@gmail.com>
2017-11-04 10:23:04 -0500

    completed step 2 of CG Education javascript Part II

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ var rating3 = 4.5;
 var rating4 = 4.8;
 var rating5 = 4.2;
 var avgRating = (rating1 + rating2 + rating3 + rating4 + rating5)/5;
-var ratings = ["4.5", "4.0", "4.5", "4.8", "4.2"];
+var teacherRatings = ["4.5", "4.0", "4.5", "4.8", "4.2"];
 avgRating = Math.round ( avgRating * 10 ) / 10;
 console.log("Teacher: " + teacher);
 console.log("Department: " + department);
-console.log("Ratings: " + ratings.join(", "));
+console.log("Ratings: " + teacherRatings.join(", "));
 console.log("Avg Rating: " + avgRating);
 var student = "Robin Reed";
 var major = "Botany"
@@ -27,3 +27,13 @@ var gpas = ["3.5", "4.0", "3.2"];
 avgGpa = Math.round( avgGpa * 10 ) / 10;
 console.log("Student GPAs: " + gpas.join(","));
 console.log("Avg Student GPA: " + avgGpa);
+var teacherRatings = [4.5, 4.0, 4.5, 4.8, 4.2];
+var teacherAvgRating = getRatingAvg(teacherRatings);
+function getRatingAvg(teacherRatings) {
+  var total = 0;
+  for (var i = 0; i < teacherRatings.length; i++) {
+  total += teacherRatings[i] / teacherRatings.length;
+}
+  return total = Math.round( total * 10 ) / 10;
+}
+console.log("Avg Rating: " + teacherAvgRating);
